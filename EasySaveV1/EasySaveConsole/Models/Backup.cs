11e7192d2f9d6998f33e470b930483ea0ad3b9b5
EzZarGG <146653268+EzZarGG@users.noreply.github.com>
2025-05-11 23:26:46 +0200

    add some english comment

--- EasySaveV1/EasySaveConsole/Models/Backup.cs
+++ EasySaveV1/EasySaveConsole/Models/Backup.cs
@@ -11,7 +11,7 @@ namespace EasySaveV1.EasySaveConsole.Models
         public string Name { get; set; }
         public string SourcePath { get; set; }
         public string TargetPath { get; set; }
-        public string Type { get; set; } // "Full" ou "Differential"
+        public string Type { get; set; } // "Full" or "Differential"
         public long FileLength { get; set; }
 
         public override string ToString() =>
