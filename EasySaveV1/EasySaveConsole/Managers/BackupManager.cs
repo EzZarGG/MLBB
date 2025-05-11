7cb52da35ea2ea91bdd08394d94265bc34f230c3
EzZarGG <146653268+EzZarGG@users.noreply.github.com>
2025-05-11 23:01:33 +0200

    Initialisation of backupManager Namespace correction on config.cs

--- EasySaveV1/EasySaveConsole/Managers/BackupManager.cs
+++ EasySaveV1/EasySaveConsole/Managers/BackupManager.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using EasySaveV1;
+using EasySaveV1.EasySaveConsole.Models;
+using EasySaveV1.EasySaveLogging;
+using System.Text.Json;
 
 namespace EasySaveV1.EasySaveConsole.Managers
 {
-    internal class BackupManager
+    public class BackupManager
     {
+        private const int MaxJobs = 5;
+        private readonly List<Backup> _jobs;
+        private readonly Logger _logger;
+        private readonly string _stateFile;
+        private readonly JsonSerializerOptions _jsonOptions;
+        private readonly Dictionary<string, StateModel> _jobStates;
+
+        public BackupManager()
+        {
+            _jobs = Config.LoadJobs();
+            _logger = Logger.GetInstance();
+            _stateFile = Config.GetStateFilePath();
+            _jsonOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            _jobStates = LoadOrInitializeStates();
+        }
     }
+
+
 }
