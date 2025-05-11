7815144166b7476491b6b6e284fdbe471f9d5789
Abdoul Karim <abdoulkarimcoulibaly525@gmail.com>
2025-05-11 23:02:48 +0200

    Add Class LogEntry

--- EasySaveV1/EasySaveLogging/Logger.cs
+++ EasySaveV1/EasySaveLogging/Logger.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
 
-namespace EasySaveV1.EasySaveLogging
+namespace EasySaveLogging.Logger
 {
-    internal class Logger
+    internal class LogEntry
     {
+        public DateTime Timestamp { get; set; }
+        public string BackupName { get; set; }
+        public string SourcePath { get; set; }
+        public string TargetPath { get; set; }
+        public long FileSize { get; set; }
+        public long TransferTime { get; set; }
+        public string Message { get; set; }
+        public string LogType { get; set; }
+        public string ActionType { get; set; } // Nouveau champ pour le type d'action
     }
+
+    
 }
