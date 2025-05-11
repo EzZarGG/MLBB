f96f913a50295b1b7fa037c67b7caf5c6a5b1b65
hacker-la-nasa <thlet0815@gmail.com>
2025-05-11 23:03:04 +0200

    creation of StateModel.cs and Program.cs

--- EasySaveV1/Program.cs
+++ EasySaveV1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using EasySaveConsole;
+using EasySaveV1.EasySaveConsole.Controllers;
+using EasySaveV1.EasySaveLogging;
+using EasySaveConsole.config;
+
+namespace EasySaveV1
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        // Init Logger
+        var logDir = Config.GetLogDirectory();
+        var logger = Logger.GetInstance();
+        var logFile = Path.Combine(
+            logDir,
+            DateTime.Today.ToString("yyyy-MM-dd") + ".json"
+        );
+        logger.SetLogFilePath(logFile);
+
+        // Ensure state file directory exists
+        var stateFilePath = Config.GetStateFilePath();
+        var stateDir = Path.GetDirectoryName(stateFilePath);
+        if (!Directory.Exists(stateDir))
+        {
+            Directory.CreateDirectory(stateDir);
+        }
+
+        // Lancer le Controller
+        var controller = new BackupController();
+        controller.Start(args);
+    }
+}
+}
